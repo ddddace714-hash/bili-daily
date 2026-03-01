298945796075a0ed35a1cf65f1ab1c8ac5d43956
ddddace714-hash <ddddace714@gmail.com>
2026-03-01 17:04:45 +0800

    Add files via upload

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = "http://122.51.25.216:3000"; // 你的后端
+// 你的服务器公网 API（必须写死 IP）
+const API_BASE = "http://122.51.25.216:3000";
 
 export const getTodayVideos = () =>
   axios.get(`${API_BASE}/api/today`).then(res => res.data);
